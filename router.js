d12dc234c321f03d03c98a1d7ac9eeec1030df2c
Marcos Gilmário <marcosgilmario@gmail.com>
2022-04-11 09:57:08 -0300

    Criação da rota de exclusão de usuário

--- router.js
+++ router.js
@@ -8,5 +8,6 @@ routes.get("/users", usersController.listUsers);
 routes.get("/users/:id", usersController.getUser);
 routes.post("/users",  usersController.createUser);
 routes.put("/users/:id", usersController.updateUser);
+routes.delete("/users/:id", usersController.deleteUser);
 
 module.exports = routes;
