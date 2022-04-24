58d95304550e4023c0d9b99a1625249d3a9dc3df
Luanatvrs <luana.lorena@mail.uft.edu.br>
2022-04-24 20:52:41 -0300

    Definindo rotas e controller dos produtos

--- migrations/20220405170854-create-users.js
+++ migrations/20220405170854-create-users.js
@@ -31,7 +31,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false
       },
-      adress: {
+      address: {
         type: Sequelize.STRING,
         allowNull: false
       },
