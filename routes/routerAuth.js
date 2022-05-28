2a88a77dac25c776ec014fa8a569608dff2deb6a
Marcos Gilmário <31623641+marcosgilmario@users.noreply.github.com>
2022-05-28 18:32:23 -0300

    feature/IT04/R51 (#32)

--- routes/routerAuth.js
+++ routes/routerAuth.js
@@ -6,5 +6,6 @@ const routes = express.Router();
 
 routes.post("/login", authController.login);
 routes.post("/register", authController.register);
+routes.post("/verify-email", authController.verifyEmail);
 
 module.exports = routes;
